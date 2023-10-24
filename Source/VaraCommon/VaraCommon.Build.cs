4ec89229f2592b0873478726a47435cf646c38c9
Alex Mason <apmason119@gmail.com>
2023-10-24 08:29:44 -0700

    Add common module

---
+++ Source/VaraCommon/VaraCommon.Build.cs
@@ -0,0 +1,16 @@
+using UnrealBuildTool;
+
+public class VaraCommon : ModuleRules
+{
+	public VaraCommon(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+		);
+	}
+}
--- Source/VaraMocap/VaraMocap.Build.cs
+++ Source/VaraMocap/VaraMocap.Build.cs
@@ -10,7 +10,7 @@ public class VaraMocap : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "VaraCommon" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
