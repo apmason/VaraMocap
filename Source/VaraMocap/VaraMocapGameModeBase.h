1fef17db19ad84ccd66c88ce1e524d73ee47b19b
Alex Mason <apmason119@gmail.com>
2023-10-23 21:12:04 -0700

    First commit

---
+++ Source/VaraMocap/VaraMocapGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VaraMocapGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VARAMOCAP_API AVaraMocapGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
