1963702bd66604c782ce13eb5862f53b03f66781
Alex Mason <apmason119@gmail.com>
2023-11-03 16:50:27 -0700

    Convert the raw data array to FString for parsing

--- Plugins/Vara/Source/Vara/Private/VaraHttpServer.cpp
+++ Plugins/Vara/Source/Vara/Private/VaraHttpServer.cpp
@@ -22,6 +22,15 @@ void FVaraHttpServer::Start()
 
 	const FHttpRequestHandler OnCallback = [&](const FHttpServerRequest& ServerRequest, const FHttpResultCallback& ResultCallback) -> bool
 	{
+		// Get the JSON as an FString
+		FString RequestBody;
+		if (ServerRequest.Body.Num() > 0)
+		{
+			RequestBody = FString(UTF8_TO_TCHAR(ServerRequest.Body.GetData()));
+
+			// parse
+		}
+		
 		UE_LOG(LogTemp, Warning, TEXT("Route is being handled!"));
 		TUniquePtr<FHttpServerResponse> Response = FHttpServerResponse::Create(TEXT("{\"success\": true}"), TEXT("application/json"));
 		ResultCallback(MoveTemp(Response));
